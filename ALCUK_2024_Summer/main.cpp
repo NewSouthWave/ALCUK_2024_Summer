9e728a578add3386d543387bcfc1c6376a2b8b1d
NewSouthWave <tigers0110@naver.com>
2024-07-01 18:34:22 +0900

    Initial Commit

---
+++ ALCUK_2024_Summer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ALCUK_2024_Summer
+//
+//  Created by Nam on 2024/07/01.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
