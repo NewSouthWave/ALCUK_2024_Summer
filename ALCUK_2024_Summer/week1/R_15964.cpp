8fa8f968dc7540cca808323fedd01ae09aed6dfd
NewSouthWave <tigers0110@naver.com>
2024-07-04 19:46:38 +0900

    BOJ 15964

---
+++ ALCUK_2024_Summer/week1/R_15964.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+//문제: https://www.acmicpc.net/problem/15964
+
+
+int main(){
+    long long a,b;
+    cin >> a >> b;
+
+    cout << (a+b)*(a-b) << "\n";
+
+    return 0;
+}
+
+
+// 참고 링크:
