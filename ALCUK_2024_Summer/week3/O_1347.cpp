88acc7e75a961826a95659e57be4e31715997cc8
NewSouthWave <tigers0110@naver.com>
2024-07-21 22:34:02 +0900

    BOJ 1347 (Comment)

--- ALCUK_2024_Summer/week3/O_1347.cpp
+++ ALCUK_2024_Summer/week3/O_1347.cpp
@@ -8,7 +8,18 @@ using namespace std;
     
 /**
  
-
+ 게임을 하는 것처럼 각 커맨드들에 대한 함수를 작성하낟
+ 크게 Left, Right, Forward 에대한 움직임에 대한 함수를 만들고
+ 플레이어에 방향에 따라 다른 동작을 하게 한다
+ 
+ bool형 차원 배열의 (0,0) 인덱스를 시작점으로 간주하며 각 커맨드에 따라 배열을 따라 움직이고
+ 한번 거쳐간 인덱스는 true 처리한다
+ 
+ 이때 주의할 점은
+ 1. 만약 행이나 열에서 0 이하의 인덱스로 향해야 하는 경우는 기존에 체크된 배열을 그대로 우측이나 아래로 밀어야한다
+ -> 이때 원래 위치는 false 처리한다
+ 2. 배열(미로)의 출력을 위해 행과 열의 크기를 알아야한다
+ 
 
  */
 
@@ -107,7 +118,7 @@ void forwardFunc() {
     else if (playerDirection == 'E') {
         coordinates.first += 1;
         mazeArr[coordinates.second][coordinates.first] = true;
-        if (coordinates.first > rowLength - 1) {    // 기존 열 길이를
+        if (coordinates.first > rowLength - 1) {    // 기존 열 길이를 넘어서는 경우만
             rowLength++;
         }
     }
---
+++ ALCUK_2024_Summer/week3/Q_9020.cpp
@@ -0,0 +1,8 @@
+//
+//  Q_9020.cpp
+//  ALCUK_2024_Summer
+//
+//  Created by Nam on 7/21/24.
+//
+
+#include <stdio.h>
